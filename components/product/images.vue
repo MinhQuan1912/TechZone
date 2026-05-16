1eabd623f1cc8c55d261e65b6db969b0334675ee
MinhQuan1912 <nguyenquan19122002@gmail.com>
2026-05-16 19:07:32 +0700

    fix image

--- components/product/images.vue
+++ components/product/images.vue
@@ -3,9 +3,10 @@
       <div class="relative aspect-square rounded-2xl overflow-hidden bg-gray-50">
          <img :src="displayMainImage" :alt="alt" class="w-full h-full object-cover transition-all duration-300" />
       </div>
+
       <div v-if="productImages.length > 1" class="flex gap-2 overflow-x-auto pb-1">
          <button v-for="img in productImages" :key="img.id"
-            class="shrink-0 w-16 h-16 rounded-xl overflow-hidden border-2 transition-all" :class="manualSelectedUrl === img.url
+            class="shrink-0 w-16 h-16 rounded-xl overflow-hidden border-2 transition-all" :class="displayMainImage === img.url
                ? 'border-primary-500 scale-95'
                : 'border-transparent hover:border-gray-300'" @click="manualSelectedUrl = img.url">
             <img :src="img.url" class="w-full h-full object-cover" />
@@ -27,8 +28,8 @@ const props = defineProps<{
 const manualSelectedUrl = ref<string | null>(null)
 
 const displayMainImage = computed(() => {
-   if (props.variantImageUrl) return props.variantImageUrl
    if (manualSelectedUrl.value) return manualSelectedUrl.value
+   if (props.variantImageUrl) return props.variantImageUrl
    const mainImg = props.productImages.find(img => img.isMain)
    if (mainImg) return mainImg.url
    return props.productImages[0]?.url || ''
